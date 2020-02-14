243ca6bfaf663954d9e302a6930f3d691fe83c84
sburakbahceci <sburakbahceci29@gmail.com>
2020-02-14 15:43:03 +0300

    Examples

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication13.cpp :  Konsol uygulamasý için giriþ noktasýný tanýmlar.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main()
+{
+	int x = 0;
+	
+	printf("%d\n" ,x);
+	printf("%d\n" ,x+1);
+	printf("%d\n", x);
+	printf("%d\n", x++);
+	printf("%d\n", x);
+	printf("%d\n", x++-1);
+	printf("%d\n", x);
+	
+	getch();
+
+    return 0;
+}
+
